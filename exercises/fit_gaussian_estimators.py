ad80800ff698488a1c182312b49a7c0273c90c73
Home <rachel.kreiter@mail.huji.ac.il>
2022-03-16 17:16:45 +0200

    practical

--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -8,29 +8,31 @@ pio.templates.default = "simple_white"
 def test_univariate_gaussian():
     # Question 1 - Draw samples and print fitted model
     samples = np.random.normal(10,1,1000)
-    uni = UnivariateGaussian(False)
+    uni = UnivariateGaussian()
     uni.fit(samples)
     print(uni.get_mu(), uni.get_var())
-    # raise NotImplementedError()
 
     # Question 2 - Empirically showing sample mean is consistent
-    ms = np.linspace(10, 100, 10).astype(np.int64)
-    absolute_distance = []
-    for m in ms:
-        X = np.random.normal(10, 1, size=m)
-        absolute_distance.append(abs(np.mean(X)-10))
-    go.Figure([go.Scatter(x=ms, y=absolute_distance)],
-              layout=go.Layout(title=r"$\text{absolute distance between the estimated- \
-                                        and true value of the expectation,\
-                                        as a function of the sample size}$")).show()
+    # ms = np.linspace(10, 1000, 100).astype(np.int64)
+    # absolute_distance = []
+    # for m in ms:
+    #     X = np.random.normal(10, 1, size=m)
+    #     absolute_distance.append(abs(np.mean(X)-10))
+    # fig = go.Figure([go.Scatter(x=ms, y=absolute_distance)],
+    #           layout=go.Layout(title=r"$\text{absolute distance between the estimated- and true value of the "
+    #                                  r"expectation, as a function of the sample size}$"))
+    #todo add axis titles
+
+    # fig.show()
               # xaxis_title="$m\\text{ - number of samples}$",
               # yaxis_title="r$|\hat\mu - mu|$",
               # height=300).show()
 
-    # raise NotImplementedError()
-
     # Question 3 - Plotting Empirical PDF of fitted model
-    # raise NotImplementedError()
+    x_axis = np.linspace(1, 1000, 1000).astype(np.int64)
+    pdfs = uni.pdf(samples)
+    fig1 = go.Figure([go.Scatter(x=x_axis, y=pdfs)], layout=go.Layout(title="jnfvjfnrvik"))
+    fig1.show()
 
 
 def test_multivariate_gaussian():
